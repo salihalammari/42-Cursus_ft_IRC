8e713794118b44983b174cdf940a6e9c20e86bd7
DayneeBoiiz <saad.ayar@hotmail.com>
2023-01-18 11:40:59 +0100

    IRC

---
+++ src/commands/CommandHandler.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CommandHandler.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sayar <sayar@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 10:05:08 by sayar             #+#    #+#             */
+/*   Updated: 2023/01/18 10:37:41 by sayar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../../includes/commands/CommandHandler.hpp"
+
+CommandHandler::CommandHandler(Server *server) : _server(server) {
+
+}
+
+CommandHandler::~CommandHandler(void) {
+
+}
+
+void	CommandHandler::request(Client *client, std::string const &message) {
+
+	std::stringstream	ssMessage(message);
+	std::string 		syntax;
+
+	while (std::getline(ssMessage, syntax)) {
+
+		syntax = syntax.substr(0, syntax[syntax.length() - 1] == '\r' ? syntax.length() - 1 : syntax.length());
+		std::string name = syntax.substr(0, syntax.find(' '));
+
+		try {
+			
+		}
+		catch (const std::out_of_range &e) {
+			client->reply(ERR_UNKNOWNCOMMAND(client->getNickName(), name));
+		}
+
+	}
+
+}
