636b913355093294b3366fb3ba14fd9a51b4010e
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2023-02-20 11:30:06 +0100

    add whois command

--- src/commands/Commands_Impl/WhoisCommand.cpp
+++ src/commands/Commands_Impl/WhoisCommand.cpp
@@ -22,21 +22,26 @@ WhoisCommand::~WhoisCommand(void)
 
 void	WhoisCommand::execute(Client *client, std::vector<std::string> arguments)
 {
-    if (arguments.size() < 1) {
+    if (arguments.size() <21) {
 		client->reply(ERR_NEEDMOREPARAMS(client->getNickName(), "WHOIS"));
 		return ;
 	}
 	
 	std::string name = argument[0];
+	std::string target = arguments[1];
 	Client *client = server->getClient(name);
-	
+	Channel *channel = _server->getChannel(target);
+
 	 if (!client)
 	 {
-		client->reply(ERR_NOSUCHNICK(client->getNickName(), "WHOIS"));
+		client->reply(ERR_NOSUCHNICK(client->getNickName(), target));
 		return ;
 	}
-RPL_WHOISUSER 
-	
-	
+client.reply(RPL_WHOISUSER(client.getNickName(), client.getUserName(), client.getPrefix));
+client.reply(RPL_WHOISSERVER);
+client.reply(RPL_WHOISMODE(client.getNickName()));	
+client.reply(RPL_ENDOFWHOIS);
     
+return;
 }
+
